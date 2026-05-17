a465b33166eabc3c28b5d0ab5889613ac8ff38c5
Dang Ngoc Hong Yen <dangngochongyen40@gmail.com>
2026-05-17 21:35:23 +0700

    ver 2

--- obj/Debug/net9.0/VLU.BloodDonation.Api.AssemblyInfo.cs
+++ obj/Debug/net9.0/VLU.BloodDonation.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VLU.BloodDonation.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b53ba4466c97507687a5b10a7d01b111ec8fc96")]
 [assembly: System.Reflection.AssemblyProductAttribute("VLU.BloodDonation.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VLU.BloodDonation.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
