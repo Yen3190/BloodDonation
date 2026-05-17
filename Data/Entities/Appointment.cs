f8fddb7509e20235a5945f81185457199396f719
Dang Ngoc Hong Yen <dangngochongyen40@gmail.com>
2026-05-17 21:47:51 +0700

    add entities

--- Data/Entities/Appointment.cs
+++ Data/Entities/Appointment.cs
@@ -6,5 +6,17 @@ public class Appointment
     public int UserId{get; set;}
     public User?User {get; set; }
     
+    public int BloodEventId {get; set;}
+    public BloodEvent? BloodEvent { get; set;}
+
+    public DateTime AppointmentTime {get; set; } //Gio hen hien mau
+    public string Status {get; set; } = "Pending"; //pending, approved, rejected
+
+    //Thong tin lam sang: Bac si cap nhat sau khi hien mau
+    public int? BloodVolumeMl {get; set; } //Luong mau hien
+    public string? CertifiedCode {get; set; } //Ma so chung nhan hien mau
+    public int? PointsAwarded {get; set; } //So diem ren luyen duoc cong
+    public DateTime? DonationDate {get; set; } //Ngay hien mau thuc te
+
 
 }
